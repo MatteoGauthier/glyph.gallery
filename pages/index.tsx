d5f681aaa2cabd4da729c5f0d103024d0acf18ea
Mattèo Gauthier <matteo.gauthier@gmail.com>
2022-12-01 11:55:27 +0100

    [UI] Add marquee + header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,58 @@
 import clsx from "clsx"
-import Head from "next/head"
-import Image from "next/image"
 import { useCallback, useState } from "react"
-import styles from "../styles/Home.module.css"
 import Char from "../components/Char"
+import Header from "../components/Header"
+import { characters } from "../utils/constants"
 
-type FontFamily = "font-sans" | "font-serif" | "font-mono"
+type Font = {
+  name: string
+  tw: string
+}
+
+const availableFonts: Font[] = [
+  {
+    name: "Inter",
+    tw: "font-inter",
+  },
+  {
+    name: "Default System",
+    tw: "font-[system-ui]",
+  },
+  {
+    name: "Sans Serif (Default)",
+    tw: "font-[sans-serif]",
+  },
+  {
+    name: "Serif (system)",
+    tw: "font-[serif]",
+  },
+
+  {
+    name: "Mono (system)",
+    tw: "font-[monospace]",
+  },
+]
 
 export default function Home() {
-  const [fontFamily, setFontFamily] = useState<FontFamily>("font-mono")
+  const [fontFamily, setFontFamily] = useState<Font>(availableFonts[0])
+
+  const changeFont = useCallback(() => {
+    const currentIndex = availableFonts.findIndex((f) => f === fontFamily)
+    const nextIndex = (currentIndex + 1) % availableFonts.length
+    setFontFamily(availableFonts[nextIndex])
+  }, [fontFamily])
 
   return (
     <div>
+      <Header />
       <main className="max-w-screen-xl mx-auto">
-        <div className={clsx("flex flex-wrap items-start", `${fontFamily}`)}>
-          {[...Array(300)].map((_, i) => (
-            <Char key={"icon" + i} text={String(i)} />
+        <div className="flex font-mono text-sm mb-2 justify-between">
+          <div></div>
+          <button onClick={changeFont}>Change font : {fontFamily.name}</button>
+        </div>
+        <div className={clsx("flex flex-wrap items-start", `${fontFamily.tw}`)}>
+          {characters.map((char, i) => (
+            <Char key={"icon" + i} text={char} />
           ))}
         </div>
       </main>
