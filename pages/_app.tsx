b2586a451b3deb9a0af055ed644052bfc3d9c863
Mattèo Gauthier <matteo.gauthier@gmail.com>
2022-12-02 02:15:16 +0100

    [APP] Improve font loading + image FCP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,30 @@ import "tailwindcss/tailwind.css"
 import type { AppProps } from "next/app"
 import { Toaster } from "react-hot-toast"
 
+import LocalFont from "@next/font/local"
+
+const inter = LocalFont({
+  src: [
+    {
+      path: "../public/fonts/Inter-Regular.woff2",
+      weight: "400",
+    },
+    {
+      path: "../public/fonts/Inter-Medium.woff2",
+      weight: "500",
+    },
+  ],
+  variable: "--font-inter",
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <style jsx global>{`
+        :root {
+          --font-inter: ${inter.style.fontFamily};
+        }
+      `}</style>
       <Toaster position="bottom-right" />
       <Component {...pageProps} />
     </>
