e965a265c09043569cb2a62e59245109f5fa3408
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-08-06 05:00:06 +0200

    [ANALYTICS] Shynet init

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,13 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <noscript>
+          <img src="https://shynet-railway-production.up.railway.app/ingress/27311224-d936-4066-92a7-7041e3fcc239/pixel.gif" />
+        </noscript>
+        <script
+          defer
+          src="https://shynet-railway-production.up.railway.app/ingress/27311224-d936-4066-92a7-7041e3fcc239/script.js"
+        ></script>
       </body>
     </Html>
   )
